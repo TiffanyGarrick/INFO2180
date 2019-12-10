4e50c11246b2a4f557105ed4a9325977c7fe6324
hollowayjenelle <55669854+hollowayjenelle@users.noreply.github.com>
2019-12-10 13:58:14 -0500

    Update newuser.php

--- newuser.php
+++ newuser.php
@@ -1,38 +1,64 @@
 <?php
-
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ob_start();
+session_start();
+$host = "localhost";
+$username = "root"; //for testing purpose
+$pword = ""; //for testing purpose
+$database = "bugme";
+$message = "";
+    if(isset($_POST['submit'])){
         //dump($_POST);
         
-        $firstname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $lastname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-        $password = password_hash(filter_input(INPUT_POST, 'password', FILTER_VALIDATE_REGEXP), PASSWORD_DEFAULT);
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
         
-        if(empty($firstname)){
-            echo 'First name is empty<br/>';
+        if(!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/" , $password)){
+            $message .= "Password is weak";
         }
         
-        if(empty($lastname)){
-            echo 'Last name is empty<br/>';
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $message .= "Email not valid";
         }
+
         
-        if(!preg_match("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0.9])(?=.{8,})")){
-            echo 'Password is weak';
-        }
-        
-        if(!$email){
-            echo 'That email is not valid!<br/>';
-        }
         
         try{
-            $conn = new PDO("mysql:host=localhost;dbname=schema", email, password);
+            $conn = new PDO("mysql:host=$host;dbname=$database", $username, $pword);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "INSERT INTO Users (FirstName, LastName, Password, Email) VALUES ($firstname, $lastname, $password, $email)";
-            $conn->exec($sql);
-            echo "New record created successfully";
+        
+               $select_stmt=$conn->prepare("SELECT * FROM users 
+                      WHERE email=:uemail"); // sql select query
+               
+               $select_stmt->execute(array(':uemail'=>$email)); //execute query 
+               $row=$select_stmt->fetch(PDO::FETCH_ASSOC); 
+               
+               if($row["email"]==$email){
+                $message .= "Email exists"; //check condition email already exists 
+               }
+               else //check no "$errorMsg" show then continue
+               {
+                 $hash = md5($password);
+                
+                $insert_stmt=$conn->prepare("INSERT INTO users (firstame, lastname, password, email) VALUES
+                            (:fname, :lname, :upassword,:uemail)");   //sql insert query     
+                
+                if($insert_stmt->execute(array( ':fname' =>$firstname, 
+                                                ':lname' =>$lastname,
+                                                ':upassword'=>$hash,
+                                                ':uemail'=>$email))){
+                         
+                 header("location('dashboardpage.php')");
+                }
+               }
         }
         catch(PDOException $e){
-            echo $sql . "<br>" . $e->getMessage();
+            $e->getMessage();
         }
-        $conn = null;
+        
+    }else{
+        echo "Submit not set";
     }
